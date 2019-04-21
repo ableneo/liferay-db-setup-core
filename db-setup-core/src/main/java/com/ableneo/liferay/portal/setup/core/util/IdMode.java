7deec526e117af5d2dd3f171359fe034e3c2feb5
Pawel Kruszewski <kruszewsky@gmail.com>
2019-04-21 20:59:58 +0200

    fix broken copyright

--- db-setup-core/src/main/java/com/ableneo/liferay/portal/setup/core/util/IdMode.java
+++ db-setup-core/src/main/java/com/ableneo/liferay/portal/setup/core/util/IdMode.java
@@ -4,7 +4,8 @@ package com.ableneo.liferay.portal.setup.core.util;
  * #%L
  * Liferay Portal DB Setup core
  * %%
- * Copyright (C) 2016 - 2019 ableneo s. r. o.
+ * Original work Copyright (C) 2016 - 2018 mimacom ag
+ * Modified work Copyright (C) 2018 - 2020 ableneo, s. r. o.
  * %%
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -12,10 +13,10 @@ package com.ableneo.liferay.portal.setup.core.util;
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
